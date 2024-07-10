381ed22a67b38987c5916062e1dccdf5458b36b0
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-10 19:52:29 +0530

    Add files via upload

---
+++ ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int intermediate;
+            int result = 0;
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+
+            while (number > 0)
+            {
+                intermediate = number % 10;
+                result = result * 10 + intermediate;
+                number = (int)number / 10;
+            }
+
+             Console.WriteLine("The Reverse of the Number {0}",result);
+
+        }
+    }
+}
