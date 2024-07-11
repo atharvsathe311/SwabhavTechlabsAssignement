4915a6f825ec45659db7e7bf862af2f66a0eeee6
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-11 22:12:15 +0530

    Add files via upload

---
+++ NumberofSpaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberofSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Sentence");
+            String userInput = Console.ReadLine();
+            int count = 0;
+
+            for (int iterator = 0; iterator < userInput.Length; iterator++)
+                if (userInput[iterator] == ' ') count++;
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
---
+++ StringPalindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String for cheking if it is Palindrome..");
+            String userInput = Console.ReadLine();
+            String revesedString = "";
+ 
+            for (int iterator = userInput.Length-1; iterator >= 0 ; iterator--) 
+            {
+                revesedString += userInput[iterator];
+            }
+
+            var toPrint = (userInput == revesedString) ? "The string " + userInput + " is a Palindrome" : "The String " + userInput + " is not a Palindrome";
+            Console.WriteLine(toPrint);
+        }
+    }
+}
