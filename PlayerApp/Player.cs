c0a64d7f52a10f96f8d32a0b6e31debc7f998b88
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-17 01:04:44 +0530

    Add files via upload

---
+++ PlayerApp/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Player
+    {
+        private int _playerId;
+        private string _playerName;
+        private int _playerAge;
+        
+        public Player(int playerId, string playerName):this(playerId, playerName, 0)
+        {
+
+        }
+        public Player(int playerId, string playerName, int playerAge) 
+        {
+            _playerId = playerId;
+            _playerName = playerName;
+            _playerAge = playerAge;
+        }
+
+        public int GetId()
+        {
+            return _playerId;
+        }
+
+        public string GetName()
+        {
+            return _playerName;
+        }
+
+        public int GetAge()
+        {
+            return _playerAge;
+        }
+        }
+    }
---
+++ PlayerApp/PlayerAppMain.cs
@@ -0,0 +1,77 @@
+﻿using PlayerApp;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool mainWhileLoop = true;
+            Player[] players = new Player[]
+            {
+                    new Player(1,"Atharv",21),
+                    new Player(2,"Shubu",10),
+                    new Player(3,"Nishank"),
+                    new Player(4,"Athul",90)
+            };
+
+            int userPlayerId;
+
+            while (mainWhileLoop)
+            {
+                Console.WriteLine("Press 1 for Entering the Product Id or 2 to Exit or 3 for getting Max Age Player");
+
+                int userChoice = int.Parse(Console.ReadLine());
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Player Id");
+                        userPlayerId = int.Parse(Console.ReadLine());
+                        Player selectedPlayer = players[userPlayerId - 1];
+
+                        Console.WriteLine("The Player's Id is {0}", selectedPlayer.GetId());
+                        Console.WriteLine("The Player's Name is {0}",selectedPlayer.GetName());
+                        Console.WriteLine("The Player's Age is {0}", selectedPlayer.GetAge());
+                        break;
+                    case 2:
+                        Console.WriteLine("Exiting......");
+                        mainWhileLoop = false;
+                        break;
+
+                    case 3:
+                        Player maxAgePlayer = WhoIsElder(players);
+                        Console.WriteLine("The Details of Player with Maximum Age is :");
+                        Console.WriteLine("Player Id :{0}",maxAgePlayer.GetId());
+                        Console.WriteLine("Player Name :{0}",maxAgePlayer.GetName());
+                        Console.WriteLine("Player Age :{0}",maxAgePlayer.GetAge());
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+
+                }
+
+
+            }
+
+        }
+
+        public static Player WhoIsElder(Player[] player)
+        {
+            int maxPlayerAge = 0;
+            Player maxAgePlayer = null;
+
+            foreach (Player selectedPlayer in player)
+            {
+                int currentPlayerAge = selectedPlayer.GetAge();
+                if (currentPlayerAge > maxPlayerAge)
+                {
+                    maxPlayerAge = currentPlayerAge;
+                    maxAgePlayer = (Player)selectedPlayer;
+                }   
+            }
+            return maxAgePlayer;
+
+        }
+    }
+}
