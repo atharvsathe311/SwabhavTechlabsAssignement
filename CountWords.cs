342f72c6b8fda3fd76b77db739b83ab775ae8d5e
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-18 19:47:54 +0530

    Add files via upload

---
+++ CountWords.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = 1;
+            string userSentence = Console.ReadLine();
+            string trimmedUserSentence = userSentence.Trim();
+
+
+
+            for (int stringIterator = 0; stringIterator < trimmedUserSentence.Length-1; stringIterator++)
+            {
+                if (trimmedUserSentence[stringIterator] == ' ')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("The Number of Words in the sentence are {0}",count);
+        }
+
+    }
+
+}
