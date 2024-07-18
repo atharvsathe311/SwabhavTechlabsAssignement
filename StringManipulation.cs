90d3f6309fd2e45e64055c84ffdaafea43c83d12
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-18 19:44:23 +0530

    Add files via upload

---
+++ StringManipulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Developer name is :{0}", args[0]);
+            Console.WriteLine("Company name is :{0}", args[1]);
+            Console.WriteLine("Locaiton is :{0}", args[2]);
+        }
+    }
+}
