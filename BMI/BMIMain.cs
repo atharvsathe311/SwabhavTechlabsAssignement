edd68ad941e8e007b1952048d021300805b105f1
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-18 19:58:40 +0530

    Add files via upload

---
+++ BMI/BMIMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[]
+            {
+                new Person(2, "Amit", 21, 6, 50),
+                new Person(1, "Atharv", 21),
+            };
+
+            foreach(Person person1 in person)
+            {
+                Print(person1);
+            }
+
+            Console.WriteLine("Max BMI is :"+ person[0].MaxBMI());
+            
+
+        }
+
+        static void Print(Person person)
+        {
+            Console.WriteLine("BMI of Person : " + person.BodyMassIndex());
+            Console.WriteLine("Body Type Person : " + person.BodyType());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BMI/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class Person
+    {
+        public const int DEFAULT_HEIGHT = 5;
+        public const int DEFAULT_WEIGHT = 50;
+        public const double CONVERSION_FACTOR  = 3.381;
+        public const double UNDER_WEIGHT_CONDITION = 18.4;
+        public const double NORMAL_WEIGHT_CONDITION = 24.9;
+        public const double OVER_WEIGHT_CONDITION = 39.9;
+        private int _id;
+        private string _name;
+        private int _age;
+        private int _height;
+        private int _weight;
+        private double _currentBMI;
+        public static double maxBMI = 0;
+
+        public Person(int id, string name, int age, int height, int weight)
+        {
+            _id = id;
+            _name = name;
+            _age = age;
+            _height = height;
+            _weight = weight;
+            
+        }
+
+        public Person(int id, string name, int age) : this(id, name, age, DEFAULT_HEIGHT, DEFAULT_WEIGHT)
+        {
+           
+        }
+
+        public double BodyMassIndex()
+        {
+            double bmiValue;
+            bmiValue = _weight / Math.Pow((_height/ 3.281),2) ;
+
+            if (bmiValue > maxBMI)
+                maxBMI = bmiValue;
+            return bmiValue;
+        }
+
+        public string BodyType()
+        {
+            double bmi = this.BodyMassIndex();
+            if (bmi <= UNDER_WEIGHT_CONDITION)
+            {
+                return "UnderWeight";
+            }
+            
+            if (UNDER_WEIGHT_CONDITION < bmi && bmi <= NORMAL_WEIGHT_CONDITION)
+            {
+                return "Normal";
+            }
+            
+            if (NORMAL_WEIGHT_CONDITION < bmi && bmi <= OVER_WEIGHT_CONDITION)
+            {
+                return "Overweight";
+            }
+            
+            return "Obese";
+        }
+
+        public double MaxBMI()
+        {
+            return maxBMI;
+        }
+    }
+}
