522a340ed24e20c5c0590f0c0396a8e86c549119
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-20 23:12:42 +0530

    Add files via upload

---
+++ StudentEncapsulation/Program.cs
@@ -0,0 +1,48 @@
+﻿using StudentEncapsulation;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student[] student = new Student[]
+            {
+                new Student{RollNumber = 1 , Name = "Atharv" , Cgpa = 9.33},
+                new Student{RollNumber = -3 , Name = "Om" , Cgpa = -2}
+            };
+
+            bool terminateCondition = true;
+
+            while (terminateCondition)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter 1 to Enter Index Number and 0 to Exit");
+                int userChoice = int.Parse(Console.ReadLine());
+
+                if (userChoice == 0)
+                {
+                    terminateCondition = false;
+                    continue;
+                }
+
+                Console.WriteLine("Enter Index Number");
+                int userInput = int.Parse(Console.ReadLine());
+
+                Student selectedStudent = student[userInput - 1];
+
+                PrintDetails(selectedStudent);
+            }
+
+
+        }
+
+        public static void PrintDetails(Student student)
+        { 
+            //Console.WriteLine(student);
+            Console.WriteLine("Student Roll Number : " + student.RollNumber);
+            Console.WriteLine("Student Name : "+student.Name);
+            Console.WriteLine("Student CGPA : "+student.Cgpa);
+        }
+    }
+}
---
+++ StudentEncapsulation/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapsulation
+{
+    class Student
+    {
+        public const int MINIMUM_CGPA = 0;
+        public const int MAXIMUM_CGPA = 10;
+
+
+        private int _rollNumber;
+        private string _name;
+        private double _cgpa;
+        public int RollNumber 
+        {
+            get 
+            {
+                return _rollNumber;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Roll Number can't be Negative");
+                }
+
+                if (value >= 0) 
+                {
+                    _rollNumber = value;
+                }
+            } 
+        }
+        public string Name 
+        { 
+            get 
+            { 
+                return _name; 
+            } 
+            set 
+            {
+                if (value.Length < 5)
+                {
+                    _name = "";
+                }
+                else
+                {
+                    _name = value;
+                }
+            } 
+        }
+        public double Cgpa 
+        {
+            get 
+            {
+                return _cgpa;
+            }
+            set 
+            {
+                if (MINIMUM_CGPA > value || MAXIMUM_CGPA < value)
+                {
+                    Console.WriteLine("Invalid CGPA");
+                }
+                else
+                {
+                    _cgpa = value;
+                }
+
+            } 
+        }
+    }
+}
